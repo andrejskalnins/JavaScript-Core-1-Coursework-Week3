032c35c7905944bb52728d78fec1c6d38b0c6409
andrejskalnins <andrejs.kalnins@gmail.com>
2021-08-10 14:25:23 +0100

    completed C-array-every exercise

--- 1-exercises/C-array-every/exercise.js
+++ 1-exercises/C-array-every/exercise.js
@@ -2,10 +2,12 @@
    This program should check if the array `group` contains only students
 */
 
-var students = ["Omar", "Austine", "Dany", "Swathi", "Lesley", "Rukmini"];
-var group = ["Austine", "Dany", "Swathi", "Daniel"];
+let students = ["Omar", "Austine", "Dany", "Swathi", "Lesley", "Rukmini"];
+let group = ["Austine", "Dany", "Swathi", "Daniel"];
 
-var groupIsOnlyStudents; // complete this statement
+groupIsOnlyStudents = group.every((member) => {
+  return students.includes(member);
+}); // complete this statement
 
 if (groupIsOnlyStudents) {
   console.log("The group contains only students");
