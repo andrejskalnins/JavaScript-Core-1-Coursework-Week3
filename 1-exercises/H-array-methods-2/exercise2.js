f90b331644d8e3fa17479a43023a217fddfec460
andrejskalnins <andrejs.kalnins@gmail.com>
2021-08-10 22:54:48 +0100

    completed H-array-methods exercises

--- 1-exercises/H-array-methods-2/exercise2.js
+++ 1-exercises/H-array-methods-2/exercise2.js
@@ -7,7 +7,11 @@
   Tip: use the string method .split() and the array method .join()
 */
 
-function capitalise(str) {}
+function capitalise(str) {
+  let modifiedString = str.split("");
+  modifiedString[0] = str[0].toUpperCase();
+  return modifiedString.join("");
+}
 
 /* 
   DO NOT EDIT BELOW THIS LINE
